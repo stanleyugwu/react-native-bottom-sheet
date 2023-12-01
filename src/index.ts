ccf961066a59c64e6220c75623d78e944fe1e965
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-12-01 10:27:24 +0100

    🐞 fix: fix typescript import intellisense bug
    Import Intellisense for `<BottomSheet/>`  uses deep component path `@devvie/bottom-sheet/lib/typescript/components/bottomSheet` instead of `@devvie/bottom-sheet`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import BottomSheet from './components/bottomSheet';
 export { default as AnimatedTouchableBackdropMask } from './components/animatedTouchableBackdropMask';
-export { default } from './components/bottomSheet';
-
 export {
   ANIMATIONS,
   CUSTOM_BACKDROP_POSITIONS,
   type BottomSheetMethods,
 } from './types.d';
 export type { BottomSheetProps } from './components/bottomSheet/types.d';
+export default BottomSheet;
