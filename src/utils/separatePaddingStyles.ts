d7bfa197f100ad39989b5aa67f481deb522014c0
Stanley Ugwu <stanleyugwu2018@gmail.com>
2024-01-05 01:10:48 +0100

    🐞 fix: make padding styles applied via `style` prop not affect drag handle component
    setting padding properties (`padding`, `paddingLeft`, `paddingVetical`, etc) in the `style` prop causes the drag handle to move position in response to the paddings.
    
    This commit fixes that and also fixes a critical issue where padding styles also caused the sheet to have extruded height making it visible even when closed

---
+++ src/utils/separatePaddingStyles.ts
@@ -0,0 +1,46 @@
+import { type ViewStyle } from 'react-native';
+import type { SheetStyleProp } from '../components/bottomSheet/types';
+
+type PadProps =
+  | 'padding'
+  | 'paddingBottom'
+  | 'paddingEnd'
+  | 'paddingHorizontal'
+  | 'paddingLeft'
+  | 'paddingRight'
+  | 'paddingStart'
+  | 'paddingTop'
+  | 'paddingVertical';
+
+type Styles = {
+  paddingStyles: Pick<ViewStyle, PadProps>;
+  otherStyles: Omit<ViewStyle, PadProps>;
+};
+
+/**
+ * Extracts and separates `padding` styles from
+ * other styles from the given `style`
+ */
+const separatePaddingStyles = (
+  style: SheetStyleProp | undefined
+): Styles | undefined => {
+  if (!style) return;
+  const styleKeys = Object.keys(style || {});
+  if (!styleKeys.length) return;
+
+  const styles: Styles = {
+    paddingStyles: {},
+    otherStyles: {},
+  };
+
+  for (const key of styleKeys) {
+    // @ts-expect-error
+    styles[key.startsWith('padding') ? 'paddingStyles' : 'otherStyles'][key] =
+      // @ts-expect-error
+      style[key];
+  }
+
+  return styles;
+};
+
+export default separatePaddingStyles;
